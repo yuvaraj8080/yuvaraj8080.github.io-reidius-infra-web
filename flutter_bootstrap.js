698f91b0359f1e559c4f8b0a53f42bcfa515a00a
yuvaraj8080 <yuvarajdekhane8080@gmail.com>
2025-03-16 11:28:33 +0530

    first commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3059705057"
+    serviceWorkerVersion: "2738522879"
   }
 });
